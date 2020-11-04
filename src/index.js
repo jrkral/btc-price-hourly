6251516a7213bca4f7dc0fc11d282dc074323d8d
Travis Duffy <travis@travisduffy.dev>
2020-11-04 15:18:02 -0400

    Converted main function to lambda function export

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const formatUSD = number => {
 	return number.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
 }
 
-const Main = async () => {
+const Main = async (req, res) => {
 	try {
 		const twitter = await twitterClient()
 		const btc = await getBTCData()
 
-		chartIcon = val => (val > 0 ? '🟢 +' : '🔴 ')
+		const chartIcon = val => (val > 0 ? '🟢 +' : '🔴 ')
 
 		const status = `1 $BTC = ${formatUSD(btc.price)} $USD
 						${chartIcon(btc.percChange)}${btc.percChange.toFixed(2)}% chg/24h\n
@@ -19,9 +19,12 @@ const Main = async () => {
 		console.log('🐦 Posting tweet...')
 		await twitter.post('statuses/update', { status })
 		console.log('🐦 Tweet posted!')
+
+		res.sendStatus(200)
 	} catch (error) {
+		res.status(500).json({ error })
 		console.error(error)
 	}
 }
 
-Main()
+exports.handler = Main
