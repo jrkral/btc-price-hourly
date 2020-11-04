d042edbffdb8998140ed2af289d4dfd6dc9b4e93
Travis Duffy <travis@travisduffy.dev>
2020-11-04 18:16:41 -0400

    Added IP to test logger output

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
 	try {
-		console.log('request received:\n', req.body)
+		console.log('request received from: ' + req.connection.remoteAddress)
 		res.status(200).send('ok')
 	} catch (error) {
 		res.status(500).send('server error')
