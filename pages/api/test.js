a42b3fff1e65ab886e1cd72557afbebc6be1f4e5
Travis Duffy <travis@travisduffy.dev>
2020-11-07 09:11:15 -0400

    Added posting method using Puppeteer

--- pages/api/test.js
+++
@@ -1,8 +0,0 @@
-export default async (req, res) => {
-	try {
-		console.log('request received from: ' + req.connection.remoteAddress)
-		res.status(200).send('ok')
-	} catch (error) {
-		res.status(500).send('server error')
-	}
-}
